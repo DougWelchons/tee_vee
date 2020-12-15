86cd87113258107e2e7ed25bc2c21954b9155b78
DougWelchons <doug.welchons@gmail.com>
2020-12-15 10:12:26 -0700

    Add refactored tests to correct typos

--- test/network_test.rb
+++ test/network_test.rb
@@ -20,10 +20,11 @@ class NetworkTest < Minitest::Test
   end
 
   def test_it_can_add_shows
+    nbc = Network.new("NBC")
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-    eslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
 
@@ -34,10 +35,11 @@ class NetworkTest < Minitest::Test
   end
 
   def test_it_has_main_characters
+    nbc = Network.new("NBC")
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-    eslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
 
@@ -48,10 +50,11 @@ class NetworkTest < Minitest::Test
   end
 
   def test_it_has_characters_by_show
+    nbc = Network.new("NBC")
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-    eslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
 
