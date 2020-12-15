a920b9edf61f61fddfb2e9123ba80aff705b7556
DougWelchons <doug.welchons@gmail.com>
2020-12-15 09:54:15 -0700

    Add initial tests for network class

---
+++ test/network_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/charcter'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+
+  def test_it_exists
+    nbc = Network.new("NBC")
+
+    assert_instance_of Network, nbc
+  end
+
+  def test_it_has_readable_attrabutes
+    nbc = Network.new("NBC")
+
+    assert_equal "NBC", network.name
+    assert_equal [], network.shows
+  end
+end
