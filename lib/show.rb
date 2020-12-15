1f3cff7499da15a0958e3feb8bdbd4f4f23d4761
DougWelchons <doug.welchons@gmail.com>
2020-12-15 09:49:02 -0700

    Add passing Show class for current tests

--- lib/show.rb
+++ lib/show.rb
@@ -6,4 +6,28 @@ class Show
     @creator = creator
     @characters = characters
   end
+
+  def total_salary
+    sum_of_salaries = 0
+    @characters.each do |character|
+      sum_of_salaries += character.salary
+    end
+    sum_of_salaries
+  end
+
+  def highest_paid_actor
+    higest_paid = @characters.first
+    @characters.each do |character|
+      if character.salary > higest_paid.salary
+        highest_paid = character
+      end
+    end
+    higest_paid.actor
+  end
+
+  def actors
+    characters.map do |character|
+      character.actor
+    end
+  end
 end
