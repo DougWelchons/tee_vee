799e54c063417883f6e1e52d672d962de657094f
DougWelchons <doug.welchons@gmail.com>
2020-12-15 09:13:11 -0700

    Add test file for charcter class

---
+++ test/charcter_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/charcter'
+
+class CharcterTest < Minitest::Test
+
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_instance_of Charcter, kitt
+  end
+
+  def test_it_has_readable_attrabutes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "KITT", kitt.name
+    assert_equal "William Daniels", kitt.actor
+    assert_equal 1000000, kitt.salary
+  end
+end
